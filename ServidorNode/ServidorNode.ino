2407f34e2e3139d8e2dbaf06b112d0368cf11095
SimonFV <47420204+SimonFV@users.noreply.github.com>
2019-05-24 21:18:56 -0600

    Update ServidorNode.ino
    Añadidas las acciones que realiza el NodeMCU para controlar todos los dispositivos en base a los datos recibidos del cliente.

--- ServidorNode/ServidorNode.ino
+++ ServidorNode/ServidorNode.ino
@@ -1,31 +1,37 @@
-#include <ESP8266WiFi.h>
-
-char ssid[] = "Familia Munoz";  //Nombre de la SSID 
-char pass[] = "patipamipatu";  //Contraseña
+#include <ESP8266WiFi.h> 
+char ssid[] = "Jicaral Sercoba";  //Nombre de la SSID 
+char pass[] = "trixie425_panda";  //Contraseña
 String data;  //String que almacena los char recibidos por el cliente concatenados
 int data2;
-
 //Se crea el servidor TCP con su respectivo puerto
 WiFiServer server(7070);
 
-void setup() {
-  pinMode(13, INPUT);
-  pinMode(12, OUTPUT);
-  pinMode(15, OUTPUT);
+void setup(){
   Serial.begin(9600);
-
+  analogWriteFreq(120); //Cambia la frecuencia de PWM
+  //Pines para controlar los motores
+  pinMode(0, OUTPUT); //Direccion: adelante/atras In-1
+  pinMode(4, OUTPUT); //In-2
+  pinMode(2, OUTPUT); //PWM, Enb-A
+  pinMode(5, OUTPUT); //Direccion: izquierda/derecha In-3
+  pinMode(16, OUTPUT); //In-4
+  pinMode(14, OUTPUT); //PWM, Enb-B
+  //Pin de la fotoresistencia
+  pinMode(13, INPUT);
+  //Pines para el registro de corrimiento
+  pinMode(12, OUTPUT);
+  pinMode(15, OUTPUT); 
+  
   //Se intenta conectar al WiFi y espera hasta que ocurra
   Serial.printf("Conectandose a %s ", ssid);
   WiFi.begin(ssid, pass);
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED){
     delay(500);
     Serial.print(".");
   }
   Serial.println("Conectado!");
   Serial.print("IP: ");
   Serial.println(WiFi.localIP());
-
   //Inicializa el servidor
   server.begin();
 }
@@ -42,17 +48,50 @@ void loop() {
         char c = client.read();
         data += c;
       }
-      //Se realizan las acciones dependiendo de la peticion del cliente
       if(data!=""){
-        data2=data.toInt();
-        shiftOut(12,15, MSBFIRST,data2);
-        int LUZ = digitalRead(13);
-        Serial.println(LUZ);
+        data2=data.toInt(); //Convierte el dato a int para procesarlo
+     
+        //Se realizan las acciones dependiendo de la peticion del cliente
+        if(data2<256){
+          shiftOut(12,15, MSBFIRST,data2); //Enciende los LEDs
+        }else if(data2==300){
+          int LUZ = digitalRead(13);
+          Serial.println(LUZ);
+          //client.write(LUZ); //Devuelve el valor de luz ambiente
+        }else if(data2==350){
+          int CARGA = analogRead(A0);
+          Serial.println(CARGA);
+          //client.write(CARGA); //Devuelve la carga de bateria
+        }else if(data2>1423){
+          data2=data2-1000;
+          digitalWrite(0, HIGH);  //Se mueve haia atras
+          digitalWrite(4, LOW);
+          analogWrite(2, data2);
+        }else if(data2>423){;
+          digitalWrite(0, LOW);   //Se mueve hacia adelante
+          digitalWrite(4, HIGH);
+          analogWrite(2, data2);
+        }else if(data2==423){
+          digitalWrite(0, LOW);   //Se detiene
+          digitalWrite(4, LOW);
+          analogWrite(2, 0);
+        }else if(data2==401){
+          digitalWrite(5, HIGH);   //Izquierda
+          digitalWrite(16, LOW);
+          analogWrite(14, 1023);
+        }else if(data2==402){
+          digitalWrite(5, LOW);   //Derecha
+          digitalWrite(16, HIGH);
+          analogWrite(14, 1023);
+        }else if(data2==400){
+          digitalWrite(5, LOW);   //Centrado
+          digitalWrite(16, LOW);
+          analogWrite(14, 0);
+        }
       }
       data = "";
       delay(10);
     }
-
     //Cierra la conexion cuando el cliente se desconecta
     client.stop();
     Serial.println("[Cliente desconectado]");
