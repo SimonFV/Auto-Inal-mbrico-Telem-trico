9a25974cce4fe13efb46bbe11402a8938b8761b8
SimonFV <47420204+SimonFV@users.noreply.github.com>
2019-05-30 02:02:13 -0600

    Update ServidorNode.ino
    Código final del programa en Arduino del esp8266.

--- ServidorNode/ServidorNode.ino
+++ ServidorNode/ServidorNode.ino
@@ -3,7 +3,7 @@
 char ssid[] = "Samsung";  //Nombre de la SSID 
 char pass[] = "simonjfv";  //Contraseña
 String data;  //String que almacena los char recibidos por el cliente concatenados
-int data2;
+int data2;  //Entero que le indica al esp8266 que hacer
 
 //Se crea el servidor TCP con su respectivo puerto
 WiFiServer server(7070);
@@ -24,7 +24,8 @@ void setup(){
   pinMode(13, INPUT);
   //Pines para el registro de corrimiento
   pinMode(12, OUTPUT);
-  pinMode(15, OUTPUT); 
+  pinMode(15, OUTPUT);
+  shiftOut(12,15, MSBFIRST,255); 
 
   //IP estatica
   IPAddress ip(192,168,43,200);     
@@ -35,25 +36,29 @@ void setup(){
   WiFi.config(ip, gateway, subnet);
   
   //Se intenta conectar al WiFi y espera hasta que ocurra
-  Serial.printf("Conectandose a %s ", ssid);
+  //Serial.printf("Conectandose a %s ", ssid);
   WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED){
     delay(500);
-    Serial.print(".");
+    //Serial.print(".");
   }
-  Serial.println("Conectado!");
-  Serial.print("IP: ");
-  Serial.println(WiFi.localIP());
+  shiftOut(12,15, MSBFIRST,63);
+  //Serial.println("Conectado!");
+  //Serial.print("IP: ");
+  //Serial.println(WiFi.localIP());
 
   //Inicializa el servidor
   server.begin();
 }
 
 void loop() {
+  if(WiFi.status() != WL_CONNECTED){
+    shiftOut(12,15, MSBFIRST,255);
+  }
   WiFiClient client = server.available();
   //Espera a que se conecte un cliente
   if (client){
-    Serial.println("\n[Cliente conectado]");
+    //Serial.println("\n[Cliente conectado]");
     //Las acciones que se realizan mientras haya un cliente conectado
     while (client.connected()){
       //Lee los datos recibidos del cliente
@@ -70,11 +75,14 @@ void loop() {
         }else if(data2==300){
           int LUZ = digitalRead(13);
           Serial.println(LUZ);
-          //client.write(LUZ); //Devuelve el valor de luz ambiente
+          client.write(LUZ); //Devuelve el valor de luz ambiente
         }else if(data2==350){
           int CARGA = analogRead(A0);
-          Serial.println(CARGA);
-          //client.write(CARGA); //Devuelve la carga de bateria
+          data = String(CARGA);
+          char data3[5];
+          data.toCharArray(data3, 5);
+          Serial.println(data3);
+          client.write(data3); //Devuelve la carga de bateria
         }else if(data2>1423){
           data2=data2-1000;
           digitalWrite(0, HIGH);  //Se mueve haia atras
@@ -89,18 +97,144 @@ void loop() {
           digitalWrite(4, LOW);
           analogWrite(2, 0);
         }else if(data2==401){
-          digitalWrite(5, HIGH);   //Izquierda
+          digitalWrite(5, HIGH);   //Derecha
           digitalWrite(16, LOW);
           analogWrite(14, 1023);
         }else if(data2==402){
-          digitalWrite(5, LOW);   //Derecha
+          digitalWrite(5, LOW);   //Izquierda
           digitalWrite(16, HIGH);
           analogWrite(14, 1023);
         }else if(data2==400){
           digitalWrite(5, LOW);   //Centrado
           digitalWrite(16, LOW);
           analogWrite(14, 0);
-        }
+        }else if(data2==360){  //CIRCULO DERECHA
+          digitalWrite(0, LOW);   //Se impulsa
+          digitalWrite(4, HIGH);
+          analogWrite(2, 1023);
+          delay(500);
+          analogWrite(2, 0);
+          digitalWrite(5, LOW);   //Gira izquierda levemente
+          digitalWrite(16, HIGH);
+          analogWrite(14, 1023);
+          delay(200);
+          digitalWrite(16, LOW);
+          digitalWrite(5, HIGH);   //Gira derecha
+          analogWrite(2, 1023);   //Acelera
+          delay(8000);
+          analogWrite(2, 0);   //Se detiene
+          analogWrite(14, 0);
+        }else if(data2==365){  //CIRCULO IZQUIERDA
+          digitalWrite(0, LOW);   //Se impulsa
+          digitalWrite(4, HIGH);
+          analogWrite(2, 1023);
+          delay(500);
+          analogWrite(2, 0);
+          digitalWrite(5, HIGH);   //Gira derecha levemente
+          digitalWrite(16, LOW);
+          analogWrite(14, 1023);  
+          delay(250);
+          digitalWrite(5, LOW);   //Gira izquierda
+          digitalWrite(16, HIGH);
+          analogWrite(2, 1023);   //Acelera
+          delay(8000);
+          analogWrite(2, 0);   //Se detiene
+          analogWrite(14, 0);
+        }else if(data2==370){  //CIRCULO INFINITO u OCHO
+          digitalWrite(0, LOW);   //Se impulsa
+          digitalWrite(4, HIGH);
+          analogWrite(2, 1023);
+          delay(500);
+          analogWrite(2, 0);
+          digitalWrite(5, LOW);   //Gira izquierda levemente
+          digitalWrite(16, HIGH);
+          analogWrite(14, 1023);
+          delay(250);
+          digitalWrite(16, LOW);
+          digitalWrite(5, HIGH);   //Gira derecha medio circulo
+          analogWrite(2, 1023);  //Acelera
+          delay(4000);
+          digitalWrite(5, LOW);   //Gira izquierda un circulo
+          digitalWrite(16, HIGH);
+          delay(8000);
+          digitalWrite(16, LOW);
+          digitalWrite(5, HIGH);   //Gira derecha medio circulo
+          delay(4000);
+          analogWrite(2, 0);   //Se detiene
+          analogWrite(14, 0);
+      }else if(data2==380){  //ZIG ZAG
+          digitalWrite(0, LOW);   //Se impulsa
+          digitalWrite(4, HIGH);
+          analogWrite(2, 1023);
+          delay(500);
+          analogWrite(2, 0);
+          digitalWrite(5, HIGH);   //Gira derecha levemente
+          digitalWrite(16, LOW);
+          analogWrite(14, 1023);
+          delay(250);
+          digitalWrite(5, LOW);   //Gira izquierda semi-circulo
+          digitalWrite(16, HIGH);
+          analogWrite(2, 1023);  //Acelera
+          delay(1000);
+          digitalWrite(16, LOW);
+          digitalWrite(5, HIGH);   //Gira derecha medio circulo
+          delay(2000);
+          digitalWrite(5, LOW);   //Gira izquierda medio circulo
+          digitalWrite(16, HIGH);
+          delay(2000);
+          digitalWrite(16, LOW);
+          digitalWrite(5, HIGH);   //Gira derecha medio circulo
+          delay(2000);
+          digitalWrite(5, LOW);   //Gira izquierda semi-circulo
+          digitalWrite(16, HIGH);
+          delay(1000);
+          analogWrite(2, 0);   //Se detiene
+          analogWrite(14, 0);
+      }else if(data2==390){  //ESPECIAL
+          digitalWrite(0, HIGH);   //Se impulsa hacia atras
+          digitalWrite(4, LOW);
+          analogWrite(2, 1023);
+          delay(500);
+          analogWrite(2, 0);
+          digitalWrite(5, HIGH);   //Gira derecha levemente
+          digitalWrite(16, LOW);
+          analogWrite(14, 1023);
+          delay(250);
+          digitalWrite(5, LOW);   //Gira izquierda
+          digitalWrite(16, HIGH);
+          analogWrite(2, 1023);  //Acelera en reversa
+          delay(2000);
+          analogWrite(2, 0);   //Se detiene
+          analogWrite(14, 0);
+          delay(1000);
+          digitalWrite(0, LOW);   //Direccion hacia adelante
+          digitalWrite(4, HIGH);
+          analogWrite(2, 1023);  //Acelera
+          delay(4000);
+          analogWrite(2, 0);   //Se detiene
+          delay(1000);
+          digitalWrite(4, LOW);
+          digitalWrite(0, HIGH);   //Direccion hacia atras
+          analogWrite(2, 1023);  //Acelera en reversa
+          delay(4000);
+          analogWrite(2, 0);   //Se detiene
+          delay(1000);
+          digitalWrite(0, LOW);   //Se impulsa
+          digitalWrite(4, HIGH);
+          analogWrite(2, 1023);
+          delay(500);
+          analogWrite(2, 0);
+          digitalWrite(5, HIGH);   //Gira derecha levemente
+          digitalWrite(16, LOW);
+          analogWrite(14, 1023);
+          delay(250);
+          digitalWrite(5, LOW);   //Gira izquierda
+          digitalWrite(16, HIGH);
+          analogWrite(2, 1023);  //Acelera
+          delay(2000);
+          analogWrite(2, 0);
+          analogWrite(14, 0);
+      }
       }
       data = "";
       delay(10);
@@ -108,6 +242,6 @@ void loop() {
 
     //Cierra la conexion cuando el cliente se desconecta
     client.stop();
-    Serial.println("[Cliente desconectado]");
+    //Serial.println("[Cliente desconectado]");
   }
 }
